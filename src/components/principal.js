4b8458ac54b0c6cb1f9b43094c084d453156ca5e
Ohrodd <wilfjuarez@gmail.com>
2022-07-02 11:58:41 -0600

    seven example gh-page

--- src/components/principal.js
+++ src/components/principal.js
@@ -1,42 +1,27 @@
+import celulares from "../products.json";
+
 function Tarjeta() {
   return(
-    <div>
-      <div className="card" style={{width: "228px"}}>
-          <img src="..." className="card-img-top" alt="..."/>
+    <div className="subcontenidoPrincipal">
+      {celulares.map((celulares)=>
+        <div className="card" style={{width: "308px", height:"550px"}}>
+          <img src={celulares.imagen} className="card-img-top" alt="..."/>
           <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">{celulares.name_product}</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="" className="btn btn-primary">Go somewhere</a>
+              <a href="" className="btn btn-primary">Ver el articulo</a>
           </div>
         </div>
+        )}
     </div>
   );
 }
 
-
-
 function Principal(){
+  console.log(celulares);
   return(
       <section className="container contenidoPrincipal">
-        <div className="subcontenidoPrincipal">
           <Tarjeta/>
-          <Tarjeta/>
-          <Tarjeta/>
-          <Tarjeta/>
-        </div>
-        <br/>
-        <div className="subcontenidoPrincipal">
-          <Tarjeta/>
-          <Tarjeta/>
-          <Tarjeta/>
-          <Tarjeta/>
-        </div>
-        <div className="subcontenidoPrincipal">
-          <Tarjeta/>
-          <Tarjeta/>
-          <Tarjeta/>
-          <Tarjeta/>
-        </div>
       </section>
   );
 }
